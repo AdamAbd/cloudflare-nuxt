eea5f604fc8e021dbc688d226aced9eb3e38480f
Adam Abdurrahman <muhammad.adam.abd@gmail.com>
2025-12-25 11:54:29 +0700

    feat: add Supabase CLI dependency and configuration files

--- app/app.vue
+++ app/app.vue
@@ -1,9 +1,9 @@
 <script setup>
-const client = useSupabaseClient()
+const supabase = useSupabaseClient()
 const user = useSupabaseUser()
 
 onMounted(async () => {
-  let { data: notes, error } = await client.from('notes').select('id')
+  let { data: notes, error } = await supabase.from('notes').select('*')
   console.log(notes, error)
 })
 </script>
@@ -16,7 +16,7 @@ onMounted(async () => {
       <p>Logged in as: {{ user.email }}</p>
     </div>
     <div v-else class="mt-4">
-      <p>Not logged in. Supabase connection: {{ client ? 'Ready' : 'Not ready' }}</p>
+      <p>Not logged in. Supabase connection: {{ supabase ? 'Ready' : 'Not ready' }}</p>
     </div>
   </div>
 </template>
